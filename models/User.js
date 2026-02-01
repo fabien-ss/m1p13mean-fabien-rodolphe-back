d291a059b44b83c4e675077b610695629ba58499
01KingMaker <fabrakoto0987@gmail.com>
2026-02-01 16:34:02 +0300

    fix: remove next on user-save async function

--- models/User.js
+++ models/User.js
@@ -16,7 +16,7 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
+  // next();
 });
 
 // Méthode pour comparer le mot de passe
