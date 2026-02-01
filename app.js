eb4a0b5dbcd034f8dfc66b14fa29183b74297c02
01KingMaker <fabrakoto0987@gmail.com>
2026-02-01 14:30:15 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test');
+
+var app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connecté"))
+  .catch(err => console.error("Erreur MongoDB:", err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/test', testRouter);
+
+module.exports = app;
